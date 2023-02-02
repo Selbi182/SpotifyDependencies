92ca7a28e36f2c496300b69ce1f84fef1b6d0c7d
Selbi <sebpet182@gmail.com>
2023-02-02 11:25:17 +0100

    Added EP/Live/Remix conditional methods + also added SpotifyOptimizedExecutorService

--- src/main/java/spotify/util/BotUtils.java
+++ src/main/java/spotify/util/BotUtils.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,12 +23,33 @@ import se.michaelthelin.spotify.model_objects.IPlaylistItem;
 import se.michaelthelin.spotify.model_objects.specification.Album;
 import se.michaelthelin.spotify.model_objects.specification.AlbumSimplified;
 import se.michaelthelin.spotify.model_objects.specification.ArtistSimplified;
+import se.michaelthelin.spotify.model_objects.specification.AudioFeatures;
 import se.michaelthelin.spotify.model_objects.specification.Image;
 import se.michaelthelin.spotify.model_objects.specification.Track;
 import se.michaelthelin.spotify.model_objects.specification.TrackSimplified;
+import spotify.services.TrackService;
+import spotify.util.data.AlbumTrackPair;
 
 public final class BotUtils {
+	private final static double EPSILON = 0.01;
 
+	private final static Pattern EP_MATCHER = Pattern.compile("\\bE\\W?P\\W?\\b");
+	private final static int EP_SONG_COUNT_THRESHOLD = 5;
+	private final static int EP_DURATION_THRESHOLD = 20 * 60 * 1000;
+	private final static int EP_SONG_COUNT_THRESHOLD_LESSER = 3;
+	private final static int EP_DURATION_THRESHOLD_LESSER = 10 * 60 * 1000;
+
+	private final static Pattern LIVE_MATCHER = Pattern.compile("\\b(LIVE|SHOW|TOUR)\\b", Pattern.CASE_INSENSITIVE);
+	private final static Pattern LIVE_MATCHER_EXTRA =
+			Pattern.compile("(\\bLIVE\\W*$|\\bLIVE.*?\\b(\\d{4}|(IN|AT|ON|PERFORMANCE|SHOW|CONCERT|SESSION))\\b)", Pattern.CASE_INSENSITIVE);
+	private final static double LIVE_SONG_COUNT_PERCENTAGE_THRESHOLD_DEFINITE = 0.9;
+	private final static double LIVENESS_THRESHOLD = 0.55;
+	private final static double LIVENESS_THRESHOLD_LESSER = 0.4;
+	private final static int LIVE_MIN_SONG_COUNT_FOR_SHORTCUT = 3;
+
+	private final static Pattern REMIX_MATCHER = Pattern.compile("\\b(RMX|REMIX+|REMIXES)\\b", Pattern.CASE_INSENSITIVE);
+	private final static double REMIX_SONG_COUNT_PERCENTAGE_THRESHOLD = 0.67;
+	private final static double REMIX_SONG_COUNT_PERCENTAGE_THRESHOLD_LESSER = 0.2;
 	/**
 	 * Utility class
 	 */
@@ -366,7 +388,9 @@ public final class BotUtils {
 	 * @return the string in the format "albumgroup_firstartistname_albumname"
 	 */
 	public static String albumIdentifierString(AlbumSimplified as) {
-		return String.join("_", as.getAlbumGroup().getGroup(), BotUtils.strippedTitleIdentifier(BotUtils.getFirstArtistName(as)), BotUtils.strippedTitleIdentifier(as.getName())).toLowerCase();
+		String artistPart = BotUtils.strippedTitleIdentifier(BotUtils.getFirstArtistName(as));
+		String releasePart = BotUtils.strippedTitleIdentifier(as.getName());
+		return String.join("_", as.getAlbumGroup().getGroup(), artistPart, releasePart).toLowerCase();
 	}
 
 	/**
@@ -434,5 +458,114 @@ public final class BotUtils {
 	 */
 	public static boolean isTrack(IPlaylistItem t) {
 		return t.getType().equals(ModelObjectType.TRACK);
+	}
+
+	/**
+	 * Returns true if the given AlbumTrackPair qualifies as EP. The definition
+	 * of an EP (on Spotify) is a SINGLE that fulfills ANY of the following attributes:
+	 * <ul>
+	 * <li>"EP" appears in the album title (uppercase, single word, may contain a
+	 * single symbol in between and after the letters)</li>
+	 * <li>min 5 songs</li>
+	 * <li>min 20 minutes</li>
+	 * <li>min 3 songs AND min 10 minutes AND doesn't have a title track*</li>
+	 * </ul>
+	 * *The great majority of EPs are covered by the first three strategies. The
+	 * last one for really silly edge cases in which an artist may release an EP
+	 * that is too similar to a slightly fancier single by numbers alone.
+	 *
+	 * @param albumTrackPair the AlbumTrackPair
+	 * @return if the AlbumTrackPair qualifies as EP
+	 */
+	public static boolean isExtendedPlay(AlbumTrackPair albumTrackPair) {
+		String albumTitle = albumTrackPair.getAlbum().getName();
+		List<TrackSimplified> tracks = albumTrackPair.getTracks();
+		if (EP_MATCHER.matcher(albumTitle).find()) {
+			return true;
+		}
+		int trackCount = tracks.size();
+		int totalDurationMs = tracks.stream().mapToInt(TrackSimplified::getDurationMs).sum();
+		if (trackCount >= EP_SONG_COUNT_THRESHOLD || totalDurationMs >= EP_DURATION_THRESHOLD) {
+			return true;
+		}
+		if (trackCount >= EP_SONG_COUNT_THRESHOLD_LESSER && totalDurationMs >= EP_DURATION_THRESHOLD_LESSER) {
+			String strippedAlbumTitle = BotUtils.strippedTitleIdentifier(albumTitle);
+			return tracks.stream()
+					.map(TrackSimplified::getName)
+					.map(BotUtils::strippedTitleIdentifier)
+					.noneMatch(t -> t.equalsIgnoreCase(strippedAlbumTitle));
+		}
+		return false;
+	}
+
+	/**
+	 * Returns true if the given release qualifies as live release. The definition
+	 * of a live release is a release that fulfills ANY of the following attributes:
+	 * <ul>
+	 * <li>At least half of the songs of this release have a combined "liveness"
+	 * average of 50% or more</li>
+	 * <li>If the word "LIVE" is included in the release title, the required
+	 * liveness threshold is reduced to 25%</li>
+	 * </ul>
+	 * The liveness value is determined by the Spotify API for each individual song.
+	 * It gives a vague idea how probable it is for the song to be live. Hints like
+	 * recording quality and audience cheers are used.
+	 *
+	 * @param albumTrackPair the AlbumTrackPair
+	 * @param trackService the TrackService required to find the liveness values
+	 * @return true if the AlbumTrackPair qualifies as Live release
+	 */
+	public static boolean isLiveRelease(AlbumTrackPair albumTrackPair, TrackService trackService) {
+		String albumTitle = albumTrackPair.getAlbum().getName();
+		List<TrackSimplified> tracks = albumTrackPair.getTracks();
+		double trackCount = tracks.size();
+		double liveTracks = tracks.stream().filter(t -> LIVE_MATCHER.matcher(t.getName()).find()).count();
+		double liveTrackPercentage = liveTracks / trackCount;
+		if (liveTrackPercentage > LIVE_SONG_COUNT_PERCENTAGE_THRESHOLD_DEFINITE) {
+			if (trackCount > LIVE_MIN_SONG_COUNT_FOR_SHORTCUT
+					|| LIVE_MATCHER_EXTRA.matcher(albumTitle).find()) {
+				return true;
+			}
+		}
+
+		boolean hasLiveInTitle = LIVE_MATCHER.matcher(albumTitle).find();
+		boolean hasLiveInTracks = liveTrackPercentage > EPSILON;
+
+		if (hasLiveInTitle || hasLiveInTracks) {
+			List<AudioFeatures> audioFeatures = trackService.getAudioFeatures(tracks);
+			double averageLiveness = audioFeatures.stream()
+					.filter(Objects::nonNull)
+					.mapToDouble(AudioFeatures::getLiveness)
+					.average()
+					.orElse(0.0);
+			boolean isLive = averageLiveness > LIVENESS_THRESHOLD;
+			if (!isLive && hasLiveInTitle) {
+				isLive = averageLiveness >= LIVENESS_THRESHOLD_LESSER;
+			}
+			return isLive;
+		}
+		return false;
+	}
+
+	/**
+	 * Returns true if the given release OR at least 2/3 of the release's tracks
+	 * have "REMIX" in their titles (one word, case-insensitive). Exception: Remix is
+	 * in the title, in which case the threshold is only 1/5.
+	 *
+	 * @param albumTrackPair the ALbumTrackPair
+	 * @return true if the given release qualifies as Remix
+	 */
+	public static boolean isRemix(AlbumTrackPair albumTrackPair) {
+		String albumTitle = albumTrackPair.getAlbum().getName();
+		List<TrackSimplified> tracks = albumTrackPair.getTracks();
+		boolean hasRemixInTitle = REMIX_MATCHER.matcher(albumTitle).find();
+		List<String> trackNames = tracks.stream().map(TrackSimplified::getName).collect(Collectors.toList());
+		double trackCountRemix = trackNames.stream().filter(t -> REMIX_MATCHER.matcher(t).find()).count();
+		double trackCount = trackNames.size();
+		double remixPercentage = trackCountRemix / trackCount;
+		if (hasRemixInTitle) {
+			return remixPercentage > REMIX_SONG_COUNT_PERCENTAGE_THRESHOLD_LESSER;
+		}
+		return remixPercentage > REMIX_SONG_COUNT_PERCENTAGE_THRESHOLD;
 	}
 }
